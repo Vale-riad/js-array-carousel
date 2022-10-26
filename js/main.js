3ef61fee94a4b3044e84b29ad0dd37e051bfcd83
Vale-riad <valentinariad91@gmail.com>
2022-10-26 18:29:14 +0200

    Aggiunge contentuto

--- js/main.js
+++ js/main.js
@@ -1,12 +1,17 @@
 const images = [ 
-    'img/01.jpg',
+     'img/01.jpg',
      'img/02.jpg',
      'img/03.jpg',
      'img/04.jpg',
      'img/05.jpg',
 ];
+console.log(images);
 
+let containerImages = document.getElementById("items");
 for (let index = 0; index < images.length; index++) {
-    
+    const img = `<div class="item active"><img src="${images[index]}"></div>`;
+    containerImages.innerHTML += img; 
     
 }
+    
+
